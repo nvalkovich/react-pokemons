c871d6de4127480777b6c5ba6206682f75fbbb9e
nvalkovich <anastasiya.valkovich@mail.ru>
2023-11-04 23:14:17 +0300

    feat: migrate App.tsx to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { Component } from 'react';
 import './App.css';
 import SearchableCardList from './components/SearchableCardList';
 import ErrorBoundary from './components/ErrorBoundary';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app-container">
-        <ErrorBoundary>
-          <SearchableCardList />
-        </ErrorBoundary>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="app-container">
+      <ErrorBoundary>
+        <SearchableCardList />
+      </ErrorBoundary>
+    </div>
+  );
 }
-
-export default App;
