249c7e4b6b5365c244b1ec60e9f20928e76d81d9
nvalkovich <anastasiya.valkovich@mail.ru>
2023-11-23 14:50:15 +0300

    feat: remove unused functions

--- src/Api.ts
+++ src/Api.ts
@@ -1,9 +1,4 @@
-import { CardData, CardsResponse, QueryParams } from './types/interfaces';
-
-const baseURL = 'https://api.pokemontcg.io/v2';
-const path = {
-  cards: '/cards',
-};
+import { QueryParams } from './types/interfaces';
 
 export const getQueryString = (queryParams: QueryParams[]): string => {
   let string = '';
@@ -15,30 +10,4 @@ export const getQueryString = (queryParams: QueryParams[]): string => {
   }
 
   return string;
-};
-
-export const searchCardsByName = async (
-  name: string,
-  page: number,
-  pageSize: number
-): Promise<CardsResponse> => {
-  const query = getQueryString([
-    { key: 'pageSize', value: `${pageSize}` },
-    { key: 'page', value: `${page}` },
-    { key: 'q', value: `name:${name.trim().replaceAll(' ', '*')}*` },
-  ]);
-  const url = `${baseURL}${path.cards}${query}`;
-
-  const response = await fetch(url);
-
-  return await response.json();
-};
-
-export const getCard = async (id: string): Promise<CardData> => {
-  const url = `${baseURL}${path.cards}/${id}`;
-
-  const response = await fetch(url);
-  const card = await response.json();
-
-  return card.data;
 };
